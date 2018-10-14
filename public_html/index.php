d59e011c1f71fcebf155b533998f3e4aa4e495fe
slk <slawomir.grochowski@gmail.com>
2018-10-14 21:56:53 +0200

    fix ini_set

--- public_html/index.php
+++ public_html/index.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-ini_set('display_startup_errors',1);
-ini_set('display_errors',1);
+ini_set('display_startup_errors','1');
+ini_set('display_errors','1');
 error_reporting(-1);
 
 header("Access-Control-Allow-Origin: *");
