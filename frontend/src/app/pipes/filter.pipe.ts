0a1d5e3f98322f2e97ec8ba3bdb59973c6930bda
slk <slawomir.grochowski@gmail.com>
2018-11-29 00:39:44 +0100

    fix main search input

--- frontend/src/app/pipes/filter.pipe.ts
+++ frontend/src/app/pipes/filter.pipe.ts
@@ -3,12 +3,12 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-    transform(items: any[], searchText: string): any[] {
+    transform(items: any[], searchText: string, tagName: string): any[] {
         if(!items) return [];
         if(!searchText) return items;
         searchText = searchText.toLowerCase();
         return items.filter( it => {
-            return it.tag_name.toLowerCase().includes(searchText);
+            return it[tagName].toLowerCase().includes(searchText);
         });
     }
 }
