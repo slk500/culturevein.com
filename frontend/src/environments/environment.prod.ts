f23a0475bda4d1d653021e2c0c6484381e44968b
slk500 <slawomir.grochowski@gmail.com>
2020-01-26 17:02:33 +0100

    env prod angular

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://culturevein.com/'
+    baseUrl: 'http://http://51.178.48.169/'
 };
--- frontend/src/environments/environment.vps.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: 'http://http://51.178.48.169/'
-};
