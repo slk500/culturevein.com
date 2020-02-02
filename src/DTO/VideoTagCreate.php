b233495492608a39f7aae5395de5146b655d3f7b
slk500 <slawomir.grochowski@gmail.com>
2020-02-02 12:05:20 +0100

    fix empty tag name DTO

--- src/DTO/VideoTagCreate.php
+++ src/DTO/VideoTagCreate.php
@@ -9,16 +9,22 @@ use Cocur\Slugify\Slugify;
 
 class VideoTagCreate
 {
-    public $video_youtube_id;
+    public string $video_youtube_id;
 
-    public $tag_name;
+    public string $tag_name;
 
-    public $tag_slug_id;
+    public string $tag_slug_id;
 
-    public $user_id;
+    public ?int $user_id;
 
     public function __construct(string $video_youtube_id, string $tag_name, ?int $user_id = null)
     {
+        $trim_tag_name =  trim($tag_name);
+
+        if (empty($trim_tag_name)){
+            throw new \Exception('Tag name is empty!');
+        }
+
         $this->video_youtube_id = $video_youtube_id;
         $this->tag_slug_id = (new Slugify())->slugify($tag_name);
         $this->tag_name = $tag_name;
