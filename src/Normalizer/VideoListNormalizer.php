0d1dbc0e3df71bd3d2d1ec6ea7badaf38ec349f8
slk500 <slawomir.grochowski@gmail.com>
2020-05-01 22:01:44 +0200

    refactor VideoListNormalize

--- src/Normalizer/VideoListNormalizer.php
+++ src/Normalizer/VideoListNormalizer.php
@@ -5,30 +5,17 @@ namespace Normalizer;
 function video_list_normalize(array $videos): array
 {
     $result = [];
-    $last_artist_slug = null;
+
     foreach ($videos as $video) {
+        $slug = $video['artist_slug'];
 
-        if (($video['artist_slug']) === $last_artist_slug) {
-            $result[array_key_last($result)]['videos'][] =
-                [
-                    'name' => $video['video_name'],
-                    'youtube_id' => $video['video_youtube_id']
-                ];
-        } else {
-            $result [] =
-                [
-                'name' => $video['artist_name'],
-                'slug' => $video['artist_slug'],
-                'videos' => [
-                    [
-                        'name' => $video['video_name'],
-                        'youtube_id' => $video['video_youtube_id']
-                    ]
-                ]
-            ];
-        }
+        $result[$slug]['name'] = $video['artist_name'];
+        $result[$slug]['slug'] = $slug;
 
-        $last_artist_slug = $video['artist_slug'];
+        $result[$slug]['videos'][] = [
+            'name' => $video['video_name'],
+            'youtube_id' => $video['video_youtube_id']
+        ];
     }
 
     return array_values($result);
