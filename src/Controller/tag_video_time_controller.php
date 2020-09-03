458ef9b69ca87476452554e6fe3fdbfb7cdc4907
slk <slawomir.grochowski@gmail.com>
2020-09-03 16:41:54 +0200

    tag_video_time_create:void

--- src/Controller/tag_video_time_controller.php
+++ src/Controller/tag_video_time_controller.php
@@ -7,18 +7,15 @@ use DTO\RequestTagVideoTimeCreate;
 use Repository\TagVideoRepository;
 use Repository\TagVideoTimeRepository;
 
-//todo should return void - change on frontend
 function tag_video_time_create(TagVideoRepository $video_tag_repository,
                                TagVideoTimeRepository $video_tag_time_repository,
-                               RequestTagVideoTimeCreate $tag_video_time_create)
+                               RequestTagVideoTimeCreate $tag_video_time_create): void
 {
     //todo should be new object
     $tag_video_time_create->video_tag_id = $video_tag_repository->find(
         $tag_video_time_create->youtube_id, $tag_video_time_create->tag_slug_id);
 
     $video_tag_time_repository->add($tag_video_time_create);
-
-    return $tag_video_time_create;
 }
 
 function tag_video_time_delete(VideoTagTimeDeleter $video_tag_time_deleter, int $video_tag_time_id, ?int $user_id): void
