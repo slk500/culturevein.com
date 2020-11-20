98a450b4119a96264656019821b2c70ba7237eac
slk <slawomir.grochowski@gmail.com>
2020-11-20 22:06:47 +0100

    setTitle & meta for TopTen component

--- frontend/src/app/home-top-ten/top-ten.component.ts
+++ frontend/src/app/home-top-ten/top-ten.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {TagService} from "../services/tag.service";
 import {VideoService} from "../services/video.service";
 import {InputService} from "../services/input.service";
+import {SeoService} from "../seo.service";
 
 @Component({
   selector: 'app-top-ten',
@@ -22,9 +23,14 @@ export class TopTenComponent implements OnInit {
 
     public searchText;
 
-    constructor(private _tagService: TagService, private _videoService: VideoService, private inputSearch: InputService) {}
+    constructor(private _tagService: TagService, private _videoService: VideoService, private inputSearch: InputService,
+                private seoService: SeoService) {}
 
     ngOnInit() {
+      this.seoService.setTitle('Music Video Database Tagged By Subject Matter | CultureVein');
+      this.seoService.setMetaDescription('A community annotated music video database on YouTube, tagged by subject matter - ' +
+        'it can be a person, object, word or phrase, scene, gesture, cliche, trope.');
+
         this._tagService.getTagsNew()
             .subscribe(data => this.tagsNew = data,
                 error => this.errorMsg = error);
